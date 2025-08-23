33264c7ce4aab8af5c6bcda9c6245c4f9d4fea40
sajib999 <eshaq.feel@gmail.com>
2025-08-23 15:39:42 +0600

    Deploy in vercel then first test

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ export default function Home() {
   return (
     <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-        <h1 className='text-5xl text-green-600'>
+        <h1 className='text-5xl text-cyan-600'>
           GitHub Actions With Docker and Full Stack
         </h1>
+        <h3 className='text-lg text-amber-600'>Ishaq Ahmed</h3>
       </main>
     </div>
   );
